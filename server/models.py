68db2921fa8178d967cf68001bdade34e6b69243
Nolan Rummel <nolan.rummel22@gmail.com>
2023-08-08 10:31:37 -0400

    tables working

--- server/models.py
+++ server/models.py
@@ -3,28 +3,13 @@ from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-# convention = {
-#     "ix": "ix_%(column_0_label)s",
-#     "uq": "uq_%(table_name)s_%(column_0_name)s",
-#     "ck": "ck_%(table_name)s_%(constraint_name)s",
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-#     "pk": "pk_%(table_name)s"
-# }
-
-# metadata = MetaData(naming_convention=convention)
-
-# db = SQLAlchemy(metadata=metadata)
-
-# Models go here!
-# class User(db.Model):
-#     pass
 
 class Greenhouse(db.Model):
     __tablename__='greenhouses'
 
     id = db.Column(db.Integer, primary_key=True)
     air_temp = db.Column(db.Integer)
-    humidity =db.Column(db.Integer)
+    humidity = db.Column(db.Integer)
 
     # relationships
     zones= db.relationship('Zone', back_populates ='greenhouse')
@@ -41,12 +26,13 @@ class Plant(db.Model):
     expected_yield=db.Column(db.Integer)
     temperature_range=db.Column(db.Integer)
     moisture_range=db.Column(db.Integer)
-    humidity_range=db.Column(db.Integer)
+    sunlight_range=db.Column(db.Integer)
     symbiotic_relations=db.Column(db.String)
 
     # relationships
-    zones= db.relationship('Zone', back_populates ='plants')
+    plant_bed= db.relationship('PlantBed', back_populates ='plants')
     greenhouse = association_proxy('plants', 'greenhouse')
+    zone= association_proxy('plants', 'zones')
 
 
 class Zone(db.Model):
@@ -64,5 +50,23 @@ class Zone(db.Model):
     plant_id=db.Column(db.Integer, db.ForeignKey('plants.id'))
 
     # relationships
-    greenhouse= db.relationship('Greenhouse', back_populates ='zone')
-    plants= db.relationship('Plant', back_populates ='zones')
+    greenhouse= db.relationship('Greenhouse', back_populates ='zones')
+    plant_beds= db.relationship('PlantBed', back_populates ='zone')
+    plants=association_proxy('zone', 'plants')
+
+
+class PlantBed(db.Model):
+    __tablename__ = 'plant_beds'
+
+    id=db.Column(db.Integer, primary_key=True)
+
+    #Foreign Keys
+    plant_id=db.Column(db.Integer, db.ForeignKey('plants.id'))
+    zone_id=db.Column(db.Integer, db.ForeignKey('zones.id'))
+
+    #relationships
+    zone= db.relationship('Zone', back_populates ='plant_beds')
+    plants= db.relationship('Plant', back_populates ='plant_bed')
+
+    # zone = association_proxy('plants', 'zones')
+    # plants = association_proxy('zones', 'plants')
