5e7b2ee11c3a2c2c65a62e64565543dffbc12af1
Sadaf Chadwick <sadafchadwick@gmail.com>
2023-08-08 13:28:32 -0500

    app.py changes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -23,13 +23,27 @@ class Greenhouses(Resource):
         greenhouses = [greenhouse.to_dict(rules='-plant_beds') for greenhouse in Greenhouse.query.all()]
         return make_response(greenhouses, 200)
 
+    def post (self):
+        try:
+            data = request.get_json()
+            new_greenhouse = Greenhouse(
+                air_temp = data['air_temp'],
+                humidity = data['humidity']
+            )
+        except ValueError:
+            return make_response({"errors": ['validation errors']}, 400)
+
+        db.session.add(new_greenhouse)
+        db.session.commit()
+        return make_response(new_greenhouse.to_dict(), 201)
+
 api.add_resource(Greenhouses, '/greenhouses')
 
 class GreenhousesById(Resource):
     def get(self, id):
         greenhouse = Greenhouse.query.filter_by(id=id).first()
         if not greenhouse:
-            return make_response({"error":"Greenhouse does not exist you nut!"},404)
+            return make_response({"error":"Greenhouse does not exist you mother forker!"},404)
         return make_response(greenhouse.to_dict())
 
 api.add_resource(GreenhousesById, '/greenhouses/<int:id>')
@@ -38,6 +52,21 @@ class Zones(Resource):
     def get(self):
         zones = [zone.to_dict() for zone in Zone.query.all()]
         return make_response(zones, 200)
+
+    def post (self):
+        try:
+            data = request.get_json()
+            new_zone = Zone(
+                date_time = data['date_time'],
+                air_temperature = data['air_temperature'],
+                humidity = data['humidity']
+            )
+        except ValueError:
+            return make_response({"errors": ['validation errors']}, 400)
+
+        db.session.add(new_zone)
+        db.session.commit()
+        return make_response(new_zone.to_dict(), 201)
 
 api.add_resource(Zones, '/zones')
 
@@ -55,13 +84,13 @@ class Plants(Resource):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(plants, 200)
 
-api.add_resource(Plants, '/[plants')
+api.add_resource(Plants, '/plants')
 
 class PlantsById(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first()
         if not plant:
-            return make_response({"error":"That plant does not exist you banana shape!"},404)
+            return make_response({"error":"That plant does not exist you banana shaped fork!"},404)
         return make_response(plant.to_dict())
 
 api.add_resource(PlantsById, '/plants/<int:id>')
