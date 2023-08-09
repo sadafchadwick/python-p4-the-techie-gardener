9618d7014cfbc03e8645e46a523da8e170babd13
Sadaf Chadwick <sadafchadwick@gmail.com>
2023-08-09 10:08:35 -0500

    nav bar added

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom"
+
+function NavBar() {
+    return (
+    <nav>
+        <NavLink to="/">HOME</NavLink>
+        <NavLink to="/aboutus">ABOUT US</NavLink>
+        <NavLink to="/greenhouses">YOUR GREENHOUSES</NavLink>
+        <NavLink to="/zones">YOUR ZONES</NavLink>
+        <NavLink to="/plants">YOUR PLANTS</NavLink>
+    </nav>
+    )
+}
+
+export default NavBar;
