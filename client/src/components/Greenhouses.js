724ce30521b146a5d4605e45eed57dd6ffed20d4
Ammar Hussain <ammar92.h@gmail.com>
2023-08-09 11:43:24 -0400

    some stuff

--- client/src/components/Greenhouses.js
+++ client/src/components/Greenhouses.js
@@ -2,20 +2,34 @@ import React, { useState, useEffect } from "react";
 
 function Greenhouses() {
   const [greenhouses, setGreenhouses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("/greenhouses")
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((data) => {
-        // console.log(data)
         setGreenhouses(data);
+        setLoading(false);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        setError(error);
+        setLoading(false);
       });
   }, []);
 
-  // console.log(greenhouses)
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <div>
