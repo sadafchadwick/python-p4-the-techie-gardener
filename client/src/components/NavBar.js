40320f88fd5e0f04c679e3cb697dd0a02cbd60d4
Sadaf Chadwick <sadafchadwick@gmail.com>
2023-08-09 17:11:45 -0500

    nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,15 +2,29 @@ import React from "react";
 import { NavLink } from "react-router-dom"
 import "../styling/navbar.css"
 
+
+const linkStyles ={ 
+        display: "inline-block",
+        padding: "5px",
+        width: "220px",
+        height: "50px",
+        border: "none",
+        outline: "none",
+        color: "red",
+        background: "white",
+        cursor: "pointer",
+        position: "relative",
+    };
+
 function NavBar() {
     return (
-    <nav className="glow-on-hover">
-        <NavLink to="/">HOME</NavLink>
-        <NavLink to="/aboutus">ABOUT US</NavLink>
-        <NavLink to="/greenhouses">YOUR GREENHOUSES</NavLink>
-        <NavLink to="/zones">YOUR ZONES</NavLink>
-        <NavLink to="/plants">YOUR PLANTS</NavLink>
-        <NavLink to="/loginsignup">LOGIN/SIGNUP</NavLink>
+    <nav>
+        <NavLink style={linkStyles} className="glow-on-hover" to="/">HOME</NavLink>
+        <NavLink style={linkStyles} className="glow-on-hover" to="/aboutus">ABOUT US</NavLink>
+        <NavLink style={linkStyles} className="glow-on-hover" to="/greenhouses">YOUR GREENHOUSES</NavLink>
+        <NavLink style={linkStyles} className="glow-on-hover" to="/zones">YOUR ZONES</NavLink>
+        <NavLink style={linkStyles} className="glow-on-hover" to="/plants">YOUR PLANTS</NavLink>
+        <NavLink style={linkStyles} className="glow-on-hover" to="/loginsignup">LOGIN/SIGNUP</NavLink>
         {/* <div className="glow-on-hover"></div> */}
     </nav>
     )
