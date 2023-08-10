4f575bfc350008078381447261aa0f7a21fde2f5
Sadaf Chadwick <sadafchadwick@gmail.com>
2023-08-10 13:39:06 -0500

    started home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 function Home() {
-    return <h1>Home Page</h1>
+    return (
+    <Routes>
+        <Route path="signup" element={ <Signup /> } />
+        <Route path="login" element={ <Login /> } />
+    </Routes>
+    );
 }
 
 export default Home;
+
+
+
+
+
+
+<h1>Home Page</h1>
